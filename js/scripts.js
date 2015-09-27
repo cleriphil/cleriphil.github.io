430c22d1d9829eaf7725501cfb41d5d90a96a227
cleriphil <cleri.phil@gmail.com>
2015-09-26 18:51:34 -0700

    refactor into one html file, add jquery to animate sections

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,19 @@ $(document).ready(function(){
     }
   });
 
+  $(".navlink").click(function(e) {
+    var num = this.id.match(/\d+/)[0];
+    if( $(this).hasClass("active")) {
+      e.preventDefault();
+    } else {
+      $(".navlink").removeClass("active");
+      $(this).addClass("active");
+      $(".sections").fadeOut('fast');
+      $("#section_" + num).fadeIn('slow');
+    }
+  });
+
+
+
+
 });
